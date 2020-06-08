327d760d471aa0165a8f05e5b0fb5d9a56c8fcc4
vivek <vivek.appu619@gmail.com>
2020-06-08 16:55:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+
+ /*ajax here */
+const url="https://jsonplaceholder.typicode.com/todos";
+
+$(document).ready(
+$.getJSON(url,renderAsTodo));
+
+function renderAsTodo(json){
+json.forEach(
+   function(list){
+       addItem(list.title,list.completed);
+   }
+);}
+function addItem(item,isCompleted){
+   
+    var checked="";
+    var completed="";
+    if(isCompleted){
+        checked="checked";
+        completed="completed";
+    }
+    let html=`<li class="${completed}">
+        <div class='form-check '>
+        <label class='form-check-label'><input class='checkbox' type='checkbox' ${checked} /> 
+        ${item}
+        <i class='input-helper'></i>
+        </label>
+        </div>
+        <i class='remove mdi mdi-close-circle-outline'></i>
+    </li>`;
+    todoListItem.append(html);
+}
+
+ /*todo part here */
+    var todoListItem = $('.todo-list');
+    var todoListInput = $('.todo-list-input');
+    var countNewCompltetedTasks=0;
+    /* event listener on todo list */
+    $('.todo-list-add-btn').on("click", function(event) {
+    event.preventDefault();
+    
+    var item = $(this).prevAll('.todo-list-input').val();
+    
+    if (item) {
+    todoListItem.append(`
+    <li>
+        <div class='form-check'>
+        <label class='form-check-label'><input class='checkbox' type='checkbox' /> 
+        ${item}
+        <i class='input-helper'></i>
+        </label>
+        </div>
+        <i class='remove mdi mdi-close-circle-outline'></i>
+    </li>`);
+    todoListInput.val("");
+    }
+    
+    });
+    /* event listener on list item */
+    todoListItem.on('change', '.checkbox', function() {
+    if ($(this).attr('checked')) {
+    $(this).removeAttr('checked');
+    countNewCompltetedTasks--;
+    } else {
+    $(this).attr('checked','checked');
+    countNewCompltetedTasks++;
+    }
+    
+    $(this).closest("li").toggleClass('completed');
+    if(countNewCompltetedTasks==5){
+        alert("Congrats. 5 Tasks have been Successfully Completed ");
+    }
+    });
+    
+    
+    todoListItem.on('click', '.remove', function() {
+    $(this).parent().remove();
+    });
+    
+    $('button[type="submit"]').on('click',redirectTologin);
+    function redirectTologin(){
+        $(location).attr('href', 'login.html');
+    }
+    
